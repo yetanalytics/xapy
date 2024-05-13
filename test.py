366875829c51316ce693dd5b40bd2c66e4e93925
Cliff Casey <cliff@yetanalytics.com>
2024-05-13 14:39:53 -0400

    kwargs

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import xapi
 
-# testing
-response = xapi.post_file('sample-xapi.json', 'http://localhost:8080/xapi', 'username', 'password')
+# testing. Use LRS details below
+response = xapi.post_file('sample-xapi.json', 'localhost', 'username', 'password', port=8080, protocol='http')
 print(response)
--- xapi.py
+++ xapi.py
@@ -7,16 +7,17 @@ def xapi_auth_header(api_key, secret_key):
     token = b64encode(f"{api_key}:{secret_key}".encode('utf-8')).decode("ascii")
     return f'Basic {token}'
 
-# post xAPI array to an LRS. Address is assumed to be protocol, host, port and path including /xapi
-def post_xapi(xapi_data, address, api_key, secret_key):
+# post xAPI array to an LRS.
+def post_xapi(xapi_data, host, api_key, secret_key, batch=50, protocol='https', path='/xapi', port=None):
+    port_string = '' if (port is None) else f':{port}'
     s = requests.Session()
     s.headers.update({'Authorization': xapi_auth_header(api_key, secret_key),
                       'X-Experience-API-Version': '1.0.3'})
-    result = s.post(f'{host}/statements', json=xapi_data)
+    result = s.post(f"{protocol}://{host}{port_string}{path}/statements", json=xapi_data)
     return result.status_code
 
 # reads a file and passes the contents as a batch to be posted to post_xapi
-def post_file(filename, address, api_key, secret_key):
+def post_file(filename, host, api_key, secret_key, **kwargs):
     f = open(filename)
     data = json.load(f)
-    return post_xapi(data, host, api_key, secret_key)
+    return post_xapi(data, host, api_key, secret_key, **kwargs)
