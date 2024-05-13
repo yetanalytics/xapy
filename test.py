663c8c46fe5f90ea73aef535fc6d126a9bab7848
Cliff Casey <cliff@yetanalytics.com>
2024-05-13 16:39:40 -0400

    added batching and kwargs

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import xapi
 
 # testing. Use LRS details below
-response = xapi.post_file('sample-xapi.json', 'localhost', 'username', 'password', port=8080, protocol='http')
+response = xapi.post_file('sample-xapi.json', 'localhost', 'username', 'password', port=8080, protocol='http', batch=5)
 print(response)
--- xapi.py
+++ xapi.py
@@ -8,13 +8,34 @@ def xapi_auth_header(api_key, secret_key):
     return f'Basic {token}'
 
 # post xAPI array to an LRS.
-def post_xapi(xapi_data, host, api_key, secret_key, batch=50, protocol='https', path='/xapi', port=None):
-    port_string = '' if (port is None) else f':{port}'
-    s = requests.Session()
-    s.headers.update({'Authorization': xapi_auth_header(api_key, secret_key),
-                      'X-Experience-API-Version': '1.0.3'})
-    result = s.post(f"{protocol}://{host}{port_string}{path}/statements", json=xapi_data)
-    return result.status_code
+def post_xapi(xapi_data, host, api_key, secret_key, 
+    batch=50, 
+    protocol='https', 
+    path='/xapi', 
+    port=None, 
+    version='1.0.3'
+):
+    with requests.Session() as s:
+        # Auth and headers
+        s.headers.update({ 
+            'Authorization': xapi_auth_header(api_key, secret_key),
+            'X-Experience-API-Version': version
+            })
+        
+        # Create URL string
+        port_string = '' if (port is None) else f':{port}'
+        address = f"{protocol}://{host}{port_string}{path}/statements"
+        
+        # POST, one batch size at a time
+        r = None
+        while (len(xapi_data) > 0):
+            payload = xapi_data[:batch]
+            r = s.post(address, json=xapi_data)
+            if r.status_code != 200:
+                return r.status_code
+            xapi_data = xapi_data[batch:]
+        return r.status_code
+    
 
 # reads a file and passes the contents as a batch to be posted to post_xapi
 def post_file(filename, host, api_key, secret_key, **kwargs):
